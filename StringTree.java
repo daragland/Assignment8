b3ac3062f2b07ef2d3844b33748cc3dacac0fb9e
daragland <50704394+daragland@users.noreply.github.com>
2019-07-05 19:42:28 -0400

    Add files via upload

--- StringTree.java
+++ StringTree.java
@@ -11,36 +11,36 @@ public class StringTree {
 	
 	public void addString(String str) 
 	{
-		rootNode = addStringPlus(str, rootNode);
+		rootNode = addStringRecursive(str, rootNode);
 	} //addString method accepts string and uses  addStringPlus method to place the string in the correct part of the tree.
 	
 	public void printTree() 
 	{
 		System.out.println("The components of the tree in alphabetical order are: ");
 		
-		printTreePlus(rootNode);
+		printTreeRecursive(rootNode);
 	}//method to print the output tree in alphabetical order, calls on printTreePlus
 	
-	private Node addStringPlus(String str, Node present) // recursive method called by addString
+	private Node addStringRecursive(String str, Node present) // recursive method called by addString
 	{
 		Node brief = new Node(str); //make new node
 		
 		if(present == null)
 			present = brief;
 		else if(str.compareTo(present.value) < 0) //if the node is not null, then compare it to current string,  if not, put the new
-			present.left = addStringPlus(str, present.left); //if it's less, put the new node to the left,
-		else present.right = addStringPlus(str, present.right); //if it's not, put it to the right
+			present.left = addStringRecursive(str, present.left); //if it's less, put the new node to the left,
+		else present.right = addStringRecursive(str, present.right); //if it's not, put it to the right
 		
 		return present;
 	}
 	
-	private void printTreePlus(Node present) // recursive method to called by printTree
+	private void printTreeRecursive(Node present) // recursive method to called by printTree
 	{
 		if(present != null) 
 		{
-			printTreePlus(present.left); //again, if the current node is not null, print the left child/leaf
+			printTreeRecursive(present.left); //again, if the current node is not null, print the left child/leaf
 			System.out.println(present.value);//print the current node
-			printTreePlus(present.right);//then print the right child/leaf
+			printTreeRecursive(present.right);//then print the right child/leaf
 		}
 	}
 
